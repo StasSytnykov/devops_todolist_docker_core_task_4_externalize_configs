677892198c66244df3e47e0ce5ad26330cef502d
Stas Sytnykov <stassytnykov@gmail.com>
2025-11-23 21:10:12 +0200

    fix: AI review

--- todolist/settings.py
+++ todolist/settings.py
@@ -64,7 +64,7 @@ databaseConfig = {
     "USER": os.environ.get("USER",""),
     "PASSWORD": os.environ.get("PASSWORD",""),
     "HOST": os.environ.get("HOST",""),
-    "PORT": os.environ.get("PORT",""),
+    "PORT": os.environ.get("PORT","3306"),
 }
 
 # Database
